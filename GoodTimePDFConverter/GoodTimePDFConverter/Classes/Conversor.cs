59300272c20ea1efb6126d9600683b6187d5db5e
João Vitor Pedon Bontempo <joao.vitor.bontempo19@gmail.com>
2021-03-09 10:19:12 -0300

    Update
    - Adicionando conversão de xlsx

--- GoodTimePDFConverter/GoodTimePDFConverter/Classes/Conversor.cs
+++ GoodTimePDFConverter/GoodTimePDFConverter/Classes/Conversor.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using GoodTimePDFConverter.Forms;
 using GoodTimePDFConverter.Properties;
+using Spire.Xls;
 using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
 
 namespace GoodTimePDFConverter.Classes
 {
@@ -21,7 +23,7 @@ namespace GoodTimePDFConverter.Classes
                 if (file.file.Extension.Contains("doc"))
                 {
                     string docOrigem = file.file.FullName;
-                    string pdfSaida = outputPath + String.Format(@"\{0}.pdf", file.file.Name);
+                    string pdfSaida = outputPath + String.Format(@"\{0}.pdf", file.file.Name.Replace(file.file.Extension, ""));
                     //Utiliza as próprias dll do wor para realizar a conversão
                     Microsoft.Office.Interop.Word.Application appWord = new Microsoft.Office.Interop.Word.Application();
                     wordDocument = appWord.Documents.Open(docOrigem);
@@ -38,6 +40,46 @@ namespace GoodTimePDFConverter.Classes
                 }
                 else
                 {
+                    Microsoft.Office.Interop.Excel.Application excelApplication = new Microsoft.Office.Interop.Excel.Application();
+                    Microsoft.Office.Interop.Excel.Workbook excelWorkBook = null;
+
+                    string paramSourceBookPath = file.file.FullName;
+                    object paramMissing = Type.Missing;
+                    string paramExportFilePath = outputPath + String.Format(@"\{0}.pdf", file.file.Name.Replace(file.file.Extension, ""));
+                    XlFixedFormatType paramExportFormat = XlFixedFormatType.xlTypePDF;
+                    XlFixedFormatQuality paramExportQuality = XlFixedFormatQuality.xlQualityStandard;
+                    bool paramOpenAfterPublish = false;
+                    bool paramIncludeDocProps = true;
+                    bool paramIgnorePrintAreas = true;
+                    object paramFromPage = Type.Missing;
+                    object paramToPage = Type.Missing;
+
+                    excelWorkBook = excelApplication.Workbooks.Open(paramSourceBookPath,
+        paramMissing, paramMissing, paramMissing, paramMissing,
+        paramMissing, paramMissing, paramMissing, paramMissing,
+        paramMissing, paramMissing, paramMissing, paramMissing,
+        paramMissing, paramMissing);
+
+                    // Save it in the target format.
+                    if (excelWorkBook != null)
+                        excelWorkBook.ExportAsFixedFormat(paramExportFormat,
+                            paramExportFilePath, paramExportQuality,
+                            paramIncludeDocProps, paramIgnorePrintAreas, paramFromPage,
+                            paramToPage, paramOpenAfterPublish,
+                            paramMissing);
+
+                    if (excelWorkBook != null)
+                    {
+                        excelWorkBook.Close(false, paramMissing, paramMissing);
+                        excelWorkBook = null;
+                    }
+
+                    // Quit Excel and release the ApplicationClass object.
+                    if (excelApplication != null)
+                    {
+                        excelApplication.Quit();
+                        excelApplication = null;
+                    }
                     return true;
                 }
             }
